5e7dab6d8f7a20b648741bd146b59b137f65a3dd
E-Lop <edmondolopez@gmail.com>
2022-07-08 14:27:34 +0200

    stampato componente header

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,21 +1,29 @@
 <template>
-    <div class="header_content">
-        <div class="logo">
-            <img src="../assets/img/Spotify_logo_without_text.svg.png" alt="Logo Spotify">
-        </div>
+  <div class="header_content">
+    <div class="logo">
+      <img
+        src="../assets/img/Spotify_logo_without_text.svg.png"
+        alt="Logo Spotify"
+      />
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: MyHeader,
+  name: 'MyHeader',
 };
 </script>
 
 <style lang="scss" scoped>
-.logo {
-    width: 10px;
-}
-</style>>
+@import '../style/variables';
 
+.header_content {
+  background-color: $main_color;
+  padding: 10px 20px;
+}
+
+.logo {
+  width: 30px;
+}
 </style>
