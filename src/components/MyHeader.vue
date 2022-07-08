90f6d5740a438d9ce2ea661ddc490f692db45c72
E-Lop <edmondolopez@gmail.com>
2022-07-08 14:13:14 +0200

    creato foglio stile + componente MyHeader

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="header_content">
+        <div class="logo">
+            <img src="../assets/img/Spotify_logo_without_text.svg.png" alt="Logo Spotify">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: MyHeader,
+};
+</script>
+
+<style lang="scss" scoped>
+.logo {
+    width: 10px;
+}
+</style>>
+
+</style>
