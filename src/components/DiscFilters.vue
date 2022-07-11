ca688490d7f4c17bc920d3c88d7486c5c0edad69
E-Lop <edmondolopez@gmail.com>
2022-07-11 14:39:26 +0200

    creata componente DiscFilters

---
+++ src/components/DiscFilters.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="dropdown_row">
+    <label for="cars">Filtra per genere musicale</label>
+    <select name="genres" id="genres"></select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DiscFilters',
+};
+</script>
+
+<style></style>
